7ec8c6b7bc34853a329a1c5422a2c718e7b0294d
Ryan Newton <rrnewton@gmail.com>
2011-02-17 16:14:55 -0500

    Fixed the bug. tail is dangerous on open OpenLists.  parMap should expect a closed list.  Otherwise, what would the semantics of parMap followed by join be!

--- Control/Monad/Par/OpenList.hs
+++ Control/Monad/Par/OpenList.hs
@@ -21,8 +21,7 @@ import Debug.Trace
 
 -- -----------------------------------------------------------------------------
 -- Open Lists -- PVars at the tail.
--- TODO: FACTOR INTO SEPARATE FILE.
-
+--
 -- These have some of the advantages of imperative lists, such as
 -- constant time appending, while retaining determinism and having
 -- O(1) access to the head of the list unlike tree-based lists (e.g. append
@@ -64,29 +63,33 @@ join (OpenList hp1 tp1) (OpenList hp2 tp2) =
     do put (tl tp1) hp2
        return (OpenList hp1 tp2)
 
+-- | Head of an OpenList.
 head :: OpenList a -> a
 head (OpenList Null _) = error "cannot take head of null OpenList"
 head (OpenList hp _)   = hd hp
 
+-- | Tail of an OpenList.  Beware, if the list contains only one
+--   element (e.g. the result of tail will be null), it must be CLOSED
+--   for tail to work.
 tail :: OpenList a -> Par (OpenList a)
+-- NOTE: We could fix this limitation by adding a length field to the OpenList.
 tail (OpenList Null _) = error "cannot take tail of null OpenList"
 tail (OpenList hp tp)  = 
   do nxt <- get (tl hp)
-     return (OpenList nxt tp)
+     case nxt of
+       Null -> return empty
+       _    -> return (OpenList nxt tp)
 
 -- Take the length of a closed OpenList.
 -- length :: OpenList a -> Par Int
 length (OpenList Null _) = return 0
-
--- length (OpenList (Cons a _) (Cons b _)) | a == b = error "length Last cell."
-length (OpenList (Cons a _) (Cons b _)) | a == b = return 1
-
+-- length (OpenList (Cons a _) (Cons b _)) | a == b = return 1
 length ls = do t   <- tail ls 
-	       trace (" got tail, doing length... ") $ return ()
 	       len <- length t
-	       trace (" got length, incrementing... "++ show len) $ return ()
 	       return (len+1)
 
+-- | Add an element to the front of an OpenList.  Works irrespective
+-- | of whether the input is closed.
 cons :: NFData a => a -> OpenList a -> Par (OpenList a)
 -- Careful, consing should not close the openlist:
 cons car (OpenList Null _) = singleton car
@@ -123,30 +126,22 @@ toList (OpenList hp _) = loop hp
        converted <- loop rest
        return (head : converted)
 
+-- Strict map over closed lists.
+-- 
 -- parMap for OpenLists does not support a nice divide-and-conquer.
 -- (Well, it would support the appending if we were willing to scan ahead to find the length.)
 -- OpenLists are not Traversable... so we can't just use Par.parMap.
 --
 -- TODO: Perhaps this should use a strategy for each element:
 -- parMapM :: NFData b => (a -> Par b) -> OpenList a -> Par (OpenList b)
-parMapM _ (OpenList Null _) = trace "====base case"$return empty
-
--- parMapM fn (OpenList (Cons a _) (Cons b _)) | a == b = error "Last cell."
-
-parMapM fn (OpenList (Cons a _) (Cons b _)) | a == b =  fn a >>= singleton 
-
+parMapM _ (OpenList Null _) = return empty
+-- parMapM fn (OpenList (Cons a _) (Cons b _)) | a == b =  fn a >>= singleton 
 parMapM fn ls = 
   do h <- spawn (fn (head ls))
      t <- tail ls
---     len <- length t
---     trace ("  length of tail "++ show len) $ return ()
-
-     h' <- trace ("getting head result..."++ show (head ls)) $ get h
---     let h' = (head ls)     
-     t2 <- trace (" parmap on tail ..." ++ show t)$ parMapM fn t
-     trace "debug2" $ return ()
-     trace ("Consing.. HEAD is "++ show h') $ 
-      cons h' t2
+     h' <- get h
+     t2 <- parMapM fn t
+     cons h' t2
 
 -- maybe the following too?
 -- parMapM_ :: (a -> Par ()) -> OpenList a -> Par () 
@@ -204,19 +199,27 @@ test_ol4 = runPar$ do ol <- fromList [1..10]
 		      return (head t2)
 
 test_ol5 :: Int
-test_ol5 = runPar$ fromList ([1..10] :: [Int]) >>= length
+test_ol5 = runPar$ fromList ([1..10] :: [Int]) >>= close >>= length
 
 test_ol6 :: [Int]
 test_ol6 = runPar$ do
   l1 <- fromList [1..10]
+  close l1
   l2 <- parMapM (return . (+ 1)) l1
   close l2
   toList l2
 
 dbg0 = debugshow$ runPar$ singleton 'a' >>= close 
-dbg1 = debugshow$ runPar$ singleton 'a' >>= tail  >>= close 
+
+-- This one is an error:
+err1 = debugshow$ runPar$ singleton 'a' >>= tail  >>= close 
+-- But this will work:
+dbg1 = debugshow$ runPar$ fromList "ab" >>= tail  >>= close 
+
 dbg2 = debugshow$ runPar$ singleton 'a' >>= close >>= tail 
-dbg3 = debugshow$ runPar$ singleton 'a' >>= tail 
+
+-- This is invalid:
+err2 = debugshow$ runPar$ singleton 'a' >>= tail 
 
 -- TODO: HUnit These.
 openlist_tests :: Test
@@ -229,10 +232,10 @@ openlist_tests =
      1                   ~=? runPar (singleton 'a' >>= close >>= length),
      1                   ~=? runPar (cons 'b' empty >>= close >>= length),
 
-     TestLabel "singleton, close"$ "Cons|Cons" ~=? dbg0,
-     TestLabel "tail then close" $ "" ~=? dbg1,
-     TestLabel "close then tail" $ "" ~=? dbg2,
-     TestLabel "tail no close"   $ "" ~=? dbg3,
+     TestLabel "singleton, close"          $ "Cons|Cons" ~=? dbg0,
+     TestLabel "tail then close - SKETCHY" $ "Cons|Cons" ~=? dbg1,
+     TestLabel "close then tail"           $ "Null|Null" ~=? dbg2,
+--     TestLabel "tail no close"   $ "" ~=? dbg3,
 
      TestLabel "tail of singleton" $ 
      0                   ~=? runPar (singleton 'a' >>= close >>= tail >>= length),
@@ -251,8 +254,8 @@ openlist_tests =
      3                   ~=? test_ol4,
 
      TestLabel "length . fromList" $ 
-     10                  ~=? test_ol5
+     10                  ~=? test_ol5,
 
---     TestLabel "" $ 
---     [2..11]             ~=? test_ol6
+     TestLabel "test parMap" $ 
+     [2..11]             ~=? test_ol6
     ]
