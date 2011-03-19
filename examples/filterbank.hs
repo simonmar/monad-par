ebfc82f66350ba3d0db63808563345f3805c908f
Ryan Newton <rrnewton@gmail.com>
2011-03-19 00:24:25 -0400

    filterbank: Changed stream representation.

--- examples/filterbank.hs
+++ examples/filterbank.hs
@@ -10,57 +10,63 @@ import Debug.Trace
 -- bufsize = 256
 bufsize = 8
 
-type Stream a = OpenList (UArray Int a)
---type Stream a = IVar (IList (UArray Int a)
+--type Stream a = OpenList (UArray Int a)
+type Stream a = IVar (IList (UArray Int a))
 
 stage :: Stream Float -> Stream Float -> Par ()
-stage Null outstrm = put (tl outstrm) Null
 stage instrm outstrm = 
-  do 
-     let input = hd instrm
-         output = amap (+100) input
-     print_$ "Executing filter stage, output: "++ show output
-     cell <- newCell output
-     -- Append to the end of the output stream:
-     put (tl outstrm) cell
-     nxt <- get (tl instrm)
-     print_ "Put result to output stream..."
-     stage nxt cell
+  do ilst <- get instrm
+     case ilst of 
+       Null -> put outstrm Null
+       Cons h t -> 
+	 do let output = amap (+100) h
+	    print_$ "Executing filter stage, output: "++ show output
+	    newtl <- new
+	    -- Append to the end of the output stream:
+	    put outstrm (Cons output newtl)
+	    print_ "Put result to output stream..."
+	    stage t newtl
 
 -- If this loops forever a serial execution of the program can fail.
 source :: Int -> Stream Float -> Par ()
 source max outstrm = loop 0 outstrm
  where 
-  loop n strm | n > max = do put (tl strm) Null
+  loop n strm | n > max = do put strm Null
                              return ()
   loop n strm = 
     do let arr = array (0,bufsize-1) [(i,fromIntegral (n+i)) | i <- [0..bufsize-1]]
-       cell <- newCell arr
-       put (tl strm) cell 
+       newtl <- new
+       put strm (Cons arr newtl)
 -- yield?
-       loop (n+bufsize) cell
+       loop (n+bufsize) newtl
 
 -- print_ msg = trace msg (return ())
 
-sample cell1 =
+sample strm =
   do 
-     print_$ "1st: "++show (hd cell1)
-     cell2 <- get (tl cell1)
-     print_$ "\n  2nd: "++show (hd cell2)
-     cell3 <- get (tl cell2)
-     print_$ "\n  3rd: "++show (hd cell3)
+     Cons h1 t1 <- get strm
+     print_$ "1st: "++show h1 
 
+     Cons h2 t2 <- get t1
+     print_$ "\n  2nd: "++show h2
+
+     Cons h3 t3 <- get t2
+     print_$ "\n  3rd: "++show h3
+     return ()
+
+--length = 
+  
 
 
 test = runPar$ 
-  do strm1 <- newCell (array (0,0) [])
+  do strm1 <- new
 --     fork$ source 10000 strm1
      fork$ source 100 strm1
 --     fork$ source 5 strm1
      sample strm1
 
      print_$ "\n Next, applying filter... "
-     strm2 <- newCell (array (0,0) [])
+     strm2 <- new
      stage strm1 strm2
 
      print_$ "\n Resulting stream: "
