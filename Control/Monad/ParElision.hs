663e78bb8bbb3de4616dd86305f184087d53c8da
Ryan Newton <rrnewton@gmail.com>
2011-05-01 22:43:34 -0400

    Changed synonyms to newtypes and added a ParClass instance inside ParElision.hs

--- Control/Monad/ParElision.hs
+++ Control/Monad/ParElision.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE RankNTypes, ImpredicativeTypes, TypeSynonymInstances  #-}
+{-# LANGUAGE RankNTypes, ImpredicativeTypes, MultiParamTypeClasses
+   #-}
+--    TypeSynonymInstances
 
 -- This is a sequential implementation of the Par monad.
 
@@ -11,17 +13,10 @@
 
 
 module Control.Monad.ParElision (
-    Par, IVar, runPar, fork,
+    Par, IVar, runParElision, fork,
     new, newFull, newFull_,
     get, put, put_,
     pval, spawn, spawn_,
-
--- runParAsync,  yield,
-
---    parMap, parMapM, parMapReduceRange, parMapReduceRangeThresh,
---    InclusiveRange(..),
---    parFor
-
   ) where
 
 import qualified Control.Monad.ParClass as PC
@@ -35,19 +30,26 @@ import GHC.IO
 
 --------------------------------------------------------------------------------
 -- Central type definitions:
-type Par a = IO a
+newtype Par a = P (IO a)
 -- newtype Par a = Par (IO a)
-type IVar a = IORef (Maybe a)
+newtype IVar a = I (IORef (Maybe a))
 
--- TODO:
--- instance PC.ParClass Par where 
---   fork = fork 
---   new  = new
---   get  = get
---   put  = put
---   put_ = put_
---   newFull  = newFull
---   newFull_ = newFull_
+unP (P x) = x
+
+-- The newtype's above were necessary for the ParClass instance below
+-- and thus we need a Monad instance as well:
+instance Monad Par where 
+  (P m) >>= f = P (m >>= unP . f)
+  return x = P (return x)
+
+instance PC.ParClass Par IVar where 
+  fork = fork 
+  new  = new
+  get  = get
+  put  = put
+  put_ = put_
+  newFull  = newFull
+  newFull_ = newFull_
 
 --------------------------------------------------------------------------------
 
@@ -59,26 +61,27 @@ put_ :: IVar a -> a -> Par ()
 
 newFull :: NFData a => a -> Par (IVar a)
 newFull_ :: a -> Par (IVar a)
-runPar :: Par a -> a
+runParElision :: Par a -> a
 
 --------------------------------------------------------------------------------
 
-fork m = do m; return ()
+fork (P m) = P$ do m; return ()
 
-new = newIORef Nothing
-newFull x  = rnf x `seq` newIORef (Just x)
-newFull_ x = newIORef (Just x)
+new = P$ newIORef Nothing                     >>= return . I
+newFull  x = rnf x `seq` P (newIORef (Just x) >>= return . I)
+newFull_ x = P$ newIORef (Just x)             >>= return . I
 
-get r = do x <- readIORef r
+get (I r) = P$
+        do x <- readIORef r
 	   case x of 
              -- TODO: Could keep track of pedigree for better errors:
 	     Nothing -> error "IVar read before written!"
 	     Just y  -> return y 
 
-put  r x = rnf x `seq` writeIORef r (Just x)
-put_ r x = writeIORef r (Just x)
+put  (I r) x = rnf x `seq` P (writeIORef r (Just x))
+put_ (I r) x = P$ writeIORef r (Just x)
 
-runPar m = 
+runParElision (P m) = 
   trace ("ParElision: Running with unsafeIO...")
   unsafePerformIO m 
 
@@ -90,6 +93,11 @@ pval :: NFData a => a -> Par (IVar a)
 spawn_ :: Par a -> Par (IVar a)
 spawn  :: NFData a => Par a -> Par (IVar a)
 
-pval a = spawn (return a)
-spawn  m = do x <- m; evaluate (rnf x); newIORef (Just x)
-spawn_ m = do x <- m; newIORef (Just x)
+pval a = spawn (P$ return a)
+spawn  (P m) = P$ do x <- m; 
+		     evaluate (rnf x); 
+		     ref <- newIORef (Just x); 
+		     return (I ref)
+spawn_ (P m) = P$ do x <- m; 
+		     ref <- newIORef (Just x); 
+		     return (I ref)
