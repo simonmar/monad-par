4724667585572737f50db32681b256789ace33c1
Ryan Newton <rrnewton@gmail.com>
2011-10-06 22:57:01 -0400

    Experimenting with a tentative class architecture.

--- Control/Monad/ParClass.hs
+++ Control/Monad/ParClass.hs
@@ -1,27 +1,42 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, CPP,
+     FlexibleInstances, UndecidableInstances
+  #-}
 
 -- A class encompassing valid Par monads.
 
+-- TODO: This should probably move to Control.Monad.Par.Class 
+
 module Control.Monad.ParClass 
   (
-    ParClass(..)
+    ParFuture(..),
+    ParIVar(..)
   )
 where
 
 import Control.DeepSeq
 import qualified Control.Monad.Par as P
 
-class Monad m => ParClass m ivar | m -> ivar where
+class Monad m => ParFuture m future | m -> future where
+  spawn  :: NFData a => m a -> m (future a)
+  spawn_ :: m a -> m (future a)
+  get    :: future a -> m a
+
+class Monad m => ParIVar m ivar | m -> ivar where
   fork :: m () -> m ()
   new  :: m (ivar a)
-  get  :: ivar a -> m a
   put_ :: ivar a -> a -> m ()
+
+  put :: NFData a => ivar a -> a -> m ()
+  put v a = deepseq a (put_ v a)
+
+  -- We would ideally like to REUSE the name get here:
+  -- But that would require that ParFuture be a superclass.
+  -- get    :: ivar a -> m a
 
   -- TODO: I think we should add yield officially:
 --  yield  :: m ()
 
   -- Extra API routines that have default implementations:
-
   newFull_ ::  a -> m (ivar a)
   -- The following is usually inefficient! 
   newFull_ a = do v <- new
@@ -31,33 +46,55 @@ class Monad m => ParClass m ivar | m -> ivar where
   newFull :: NFData a => a -> m (ivar a)
   newFull a = deepseq a (newFull a)
 
-  put :: NFData a => ivar a -> a -> m ()
-  put v a = deepseq a (put_ v a)
-
-  pval :: NFData a => a -> m (ivar a)
-  pval a = spawn (return a)
-
-  spawn :: NFData a => m a -> m (ivar a)
-  spawn p = do r <- new
-	       fork (p >>= put r)
-	       return r
-
-  spawn_ :: m a -> m (ivar a)
-  spawn_ p = do r <- new
-		fork (p >>= put_ r)
-		return r
+class Monad m => ParDist m ivar | m -> ivar where
+  longSpawn :: NFData a => m a -> m (ivar a)
 
 
-instance ParClass P.Par P.IVar where 
---  runPar = P.runPar
+class Monad m => ParDistIVar m ivar | m -> ivar where
+  longFork  :: m () -> m ()
+
+
+
+instance ParIVar P.Par P.IVar where 
   fork = P.fork 
   new  = P.new
-  get  = P.get
   put  = P.put
   put_ = P.put_
   newFull  = P.newFull
   newFull_ = P.newFull_
 --  yield = P.yield
+--  get  = P.get
+
+----------------------------------------------------------------------------------------------------
+
+#if 1
+instance ParIVar m var => ParFuture m var where 
+  spawn p = do r <- new
+	       fork (p >>= put r)
+	       return r
+  spawn_ p = do r <- new
+		fork (p >>= put_ r)
+		return r
+#endif
+
+----------------------------------------------------------------------------------------------------
+
+-- t1 :: P.Par Int
+-- If the ParIVar => ParFuture instance exists the following is sufficient:
+-- t1 :: ParIVar m v => m Int
+t1 :: (ParIVar m v, ParFuture m v) => m Int
+t1 = do 
+  x <- spawn (return 3)
+  get x
+
+t2 :: (ParIVar m v) => m Int
+t2 = do 
+  x <- new
+  put x "hi"
+  return 3
+--  get x
+
+
 
 
 -- TODO: SPECIALIZE generic routines for the default par monad (and possibly ParRNG)?
