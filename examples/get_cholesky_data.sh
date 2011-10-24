dd75833ea431c1596d320266eff4af6d18dd8b28
Ryan Newton <rrnewton@gmail.com>
2011-10-24 17:35:02 -0400

    Minor: update url for get_cholesky_data.sh

--- examples/get_cholesky_data.sh
+++ examples/get_cholesky_data.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-wget http://people.csail.mit.edu/~newton/haskell-cnc/datasets/cholesky_matrix_data.tbz
+
+# wget http://people.csail.mit.edu/~newton/haskell-cnc/datasets/cholesky_matrix_data.tbz
+wget http://cs.indiana.edu/~rrnewton/datasets/cholesky_matrix_data.tbz
+
