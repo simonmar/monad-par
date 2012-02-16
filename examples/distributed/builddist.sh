c244d0b6e0d77d70852bbf72b88b6f8babc89aec
Ryan Newton <rrnewton@gmail.com>
2012-02-16 11:38:02 -0500

    Minor: remove unneeded scripts in parfib_dist dir

--- examples/distributed/builddist.sh
+++ examples/distributed/builddist.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e 
-set -x
 
 # Comment to disable debug mode:
 if [ "$DEBUG" != "" ]; then 
@@ -11,9 +10,9 @@ else
 fi
 
 ghc -i../.. -hide-package remote --make parfib_dist.hs -o parfib_dist.exe -O2 -threaded -rtsopts $OPTS $@
-# -fforce-recomp 
-cp parfib_dist.exe worker/
 
-echo "Next run this command here and in the worker/ dir:"
-echo "./parfib_dist monad 10 +RTS -N2"
-
+echo "  Next run this command here:"
+echo "MACHINE_LIST=... ./parfib_dist.exe master 10 +RTS -N2"
+echo 
+echo "  And run this somewhere else:"
+echo "./parfib_dist.exe slave +RTS -N2"
