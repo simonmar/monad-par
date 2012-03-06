47cfe5d72baa6b511f44710f6599c27d63d4d0c9
Ryan Newton <rrnewton@gmail.com>
2012-03-06 00:17:30 -0500

    update submodule

---
+++ examples/hive_run_basic_haskell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export SCHEDS="Trace Direct Sparks SMP"
+export GHC_RTS="-qa -A1M" 
+export BENCHLIST=benchlist_server.txt  
+export KEEPGOING=1 
+export GHC=ghc-7.4.1 
+export THREADS="1 2 4 8 12 16 20 24 28 32" 
+# export THREADS="1 2 4 8 12 16 20 24 28 32" 
+
+make bench
+./benchmark.run --par +RTS -N
+
+unlock_for_experiments
