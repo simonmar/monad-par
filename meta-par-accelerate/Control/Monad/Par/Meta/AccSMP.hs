3554a71f4ccfbbf58a549d34b9d975101296c727
Ryan Newton <rrnewton@gmail.com>
2012-04-22 22:56:10 -0400

    Attempting to introduce a newtype for meta-par-accelerate.  Not presently finding a working way to flip the multiparam type class arguments.

--- meta-par-accelerate/Control/Monad/Par/Meta/AccSMP.hs
+++ meta-par-accelerate/Control/Monad/Par/Meta/AccSMP.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, ConstraintKinds, FlexibleContexts, MultiParamTypeClasses #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module Control.Monad.Par.Meta.AccSMP (
   -- * Running `Par` computations using Accelerate and SMP parallelism.
-    runPar
+    Par
+  , runPar
   , runParIO
   
 --  , module Accelerate
@@ -15,23 +17,37 @@ module Control.Monad.Par.Meta.AccSMP (
   , Accelerate.unsafeHybridIArray
   , Accelerate.unsafeHybridVector
   
-  , module Control.Monad.Par.Meta
+--  , module Control.Monad.Par.Meta
+  , Meta.IVar
 ) where
 
--- TODO - NEWTYPE
-
 import Data.Monoid
-
-import Control.Monad.Par.Meta
+import qualified Control.Monad.Par.Class as PC
+import qualified Control.Monad.Par.Meta as Meta 
 import qualified Control.Monad.Par.Meta.Resources.Accelerate as Accelerate
 import qualified Control.Monad.Par.Meta.Resources.SharedMemory as SharedMemory
+import GHC.Conc (numCapabilities)
 
 tries :: Int
-tries = 20
+tries = numCapabilities
 
+-- We use Constraint kinds just to flip the arguments here:
+type ParFutureFlipped p = (PC.ParFuture p Meta.IVar)
+                    
+foo :: ParFutureFlipped p => p a -> Int
+foo = undefined
+
+-- | A `Par` monad supporting only SMP and GPU resources.
+newtype Par a = AccSMPPar (Meta.Par a)
+ deriving (Monad, Functor)
+          -- NOT MonadIO
+
+-- instance PC.ParFuture Par Meta.IVar where
+
+resource :: Meta.Resource
 resource = SharedMemory.mkResource tries <> Accelerate.mkResource
 
-runPar   :: Par a -> a
-runParIO :: Par a -> IO a
-runPar   = runMetaPar   resource
-runParIO = runMetaParIO resource
+runPar   :: Meta.Par a -> a
+runParIO :: Meta.Par a -> IO a
+runPar   = Meta.runMetaPar   resource
+runParIO = Meta.runMetaParIO resource
