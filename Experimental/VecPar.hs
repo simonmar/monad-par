84612a36656a97c151a3000ab5973f7b4486a6d9
Ryan Newton <rrnewton@gmail.com>
2012-10-26 12:40:06 -0400

    Updates from lindsey and ryans recent hacking

--- Experimental/VecPar.hs
+++ Experimental/VecPar.hs
@@ -13,14 +13,16 @@ module VecPar
        where
 
 import Control.Monad
-import Control.Monad.Par.IO (ParIO,IVar)
+import Control.Monad.IO.Class
+import Control.Monad.Par.IO (ParIO,runParIO,IVar)
 import qualified Control.Monad.Par.Class as PC
 -- import Control.Monad.Trans
 -- import qualified Control.Monad.Trans.State.Lazy as SL
 import qualified Control.Monad.Trans.State.Strict as S
 import Control.Monad.ST
 import Data.STRef
-import Data.Vector.Mutable
+import Data.Vector.Mutable as MV 
+import System.IO.Unsafe
 -- import GHC.IO (unsafeSTToIO)
 import Control.Monad.Trans (lift)
 
@@ -30,11 +32,26 @@ newtype ParVec s elt a = ParVec ((S.StateT (STVector s elt) ParIO) a)
  deriving Monad
 
 
+runParVec :: forall a elt . (forall s . ParVec s elt a) -> a
+runParVec (ParVec s) = unsafePerformIO io
+ where
+   io = do vec <- MV.new 0 -- :: IO (STVector RealWorld elt)
+           let x = S.runStateT s vec
+           (a,_) <- runParIO x
+           return a
+           
+--   x :: ParIO (a, STVector s0 elt)
+--   x = S.runStateT s (error "need a vec here")
+--   (y, _) = unsafePerformIO $ runParIO x
+   
+
 getTheVec :: ParVec s elt (STVector s elt)
-getTheVec  = undefined
+getTheVec  = ParVec S.get
 
 initTheVec :: Int -> ParVec s elt ()
-initTheVec = undefined
+initTheVec size = do
+  vec <- liftST $ MV.new size
+  ParVec$ S.put vec
 
 forkWithVec :: Int
             -> (forall sleft  . ParVec sleft elt a)
@@ -51,7 +68,7 @@ forkWithVec mid (ParVec lef) (ParVec rig) = ParVec $ do
             return ()) :: ParIO ())
   S.put b
   rig
-  lift (PC.get undefined) -- READ THE IVAR HERE
+--  lift (PC.get undefined) -- READ THE IVAR HERE
           
   S.put v
   return undefined
@@ -59,7 +76,7 @@ forkWithVec mid (ParVec lef) (ParVec rig) = ParVec $ do
 
 
 liftST :: ST s a -> ParVec s elt a
-liftST st = undefined
+liftST st = ParVec $ liftIO io
   where
     io = unsafeSTToIO st
 
@@ -67,8 +84,10 @@ liftST st = undefined
 -- instance Monad (ParVec s) where
   
 instance PC.ParFuture IVar (ParVec s elt) where
-
+-- Implement me...
+  
 instance PC.ParIVar IVar (ParVec s elt) where
+-- Implement me...
 
 
 --------------------------------------------------------------------------------
@@ -90,14 +109,20 @@ p2 = do
 
   forkWithVec 5
      (do v1 <- getTheVec
-         liftST$ write v1 2 33.3)
+--         liftST$ write v1 9 0 -- BAD! out of bounds
+         liftST$ write v1 2 33.3
+     )
      (do v2 <- getTheVec
---          liftST$ read v 25  -- BAD!
+--         liftST$ read v 2  -- BAD!
+--         liftST$ readSTRef r
          liftST$ write v2 2 44.4)
      
   x <- liftST$ read v 2
   y <- liftST$ read v 7
 
-  liftST$ writeSTRef r "hello"
-  liftST$ readSTRef r
+  liftST$ writeSTRef r "hello "
+  hello <- liftST$ readSTRef r
+
+  return$ hello ++ show (x,y)
+  
 
