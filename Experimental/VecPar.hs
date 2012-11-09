f1ee7af263761f86ab2604342bd48243231465a3
Lindsey Kuper <lindsey@rockstargirl.org>
2012-11-08 20:24:58 -0500

    Lots of comments and cleanup.

--- Experimental/VecPar.hs
+++ Experimental/VecPar.hs
@@ -5,7 +5,7 @@
 
 module VecPar
        -- (
-       --   forkWithVec, getTheVec, initTheVec, 
+       --   forkParVec, getParVec, initParVec, 
        --   liftST,
 
        --   p1
@@ -25,34 +25,55 @@ import Data.Vector.Mutable as MV
 import System.IO.Unsafe
 -- import GHC.IO (unsafeSTToIO)
 import Control.Monad.Trans (lift)
-
 import Prelude hiding (read, length)
 
+-- | The ParVec monad.  It uses the StateT monad transformer to layer
+-- a state of type (STVector s elt), on top of an inner monad, ParIO.
+-- Its third parameter, 'a', is the type inside the ParVec
+-- computation, and the 'elt' parameter is the element type of the
+-- vector.  The 's' parameter is what's known as a "phantom type".
 newtype ParVec s elt a = ParVec ((S.StateT (STVector s elt) ParIO) a)
  deriving Monad
 
-
+-- | Here, runParVec has a rank-2 type, and the phantom type 's' is
+-- bound by the inner forall quantifier.
 runParVec :: forall a elt . (forall s . ParVec s elt a) -> a
-runParVec (ParVec s) = unsafePerformIO io
+-- Here we're just using the ParVec value constructor tag to
+-- destructure the argument to runParVect.  The result is
+-- unsafePerformIO lets us get the needed 'a' out of an IO
+-- computation.  The 'st' in (ParVec st) is of the type ((S.StateT
+-- (STVector s elt) ParIO) a).
+runParVec (ParVec st) = unsafePerformIO io
  where
+   -- Create a new mutable vector of length 0 and do a runStateT with
+   -- it, getting back a monadic value of type ParIO, which we then
+   -- run, getting a value and a final state.  We keep the value and
+   -- throw away the state.
    io = do vec <- MV.new 0 -- :: IO (STVector RealWorld elt)
-           let x = S.runStateT s vec
+           let x = S.runStateT st vec -- x :: ParIO (a, STVector s0 elt)
            (a,_) <- runParIO x
            return a
-           
---   x :: ParIO (a, STVector s0 elt)
---   x = S.runStateT s (error "need a vec here")
---   (y, _) = unsafePerformIO $ runParIO x
-   
 
-getTheVec :: ParVec s elt (STVector s elt)
-getTheVec  = ParVec S.get
+-- | getParVec just uses the 'get' function from
+-- Control.Monad.Trans.State.Strict to get the current value of the
+-- state within the monad.  Here the thing we want to get back is the
+-- state of the vector itself, so we instantiate 'a' with its type,
+-- (STVector s elt).
+getParVec :: ParVec s elt (STVector s elt)
+getParVec  = ParVec S.get
 
-initTheVec :: Int -> ParVec s elt ()
-initTheVec size = do
+-- | initParVec creates a new mutable vector and returns a ParVec with
+-- that new mutable vector in it.
+initParVec :: Int -> ParVec s elt ()
+initParVec size = do
   vec <- liftST $ MV.new size
-  ParVec$ S.put vec
+  ParVec $ S.put vec
 
+-- | forkWithVec takes a split point and two ParVec computations.  It
+-- gets the state of the current computation, which is a vector, and
+-- then divides up that state between the two other computations.
+-- Writes to those two computations actually mutate the original
+-- vector.
 forkWithVec :: Int
             -> (forall sleft  . ParVec sleft elt a)
             -> (forall sright . ParVec sright elt b)
@@ -70,6 +91,9 @@ forkWithVec mid (ParVec lef) (ParVec rig) = ParVec $ do
   rig
 --  lift (PC.get undefined) -- READ THE IVAR HERE
           
+  -- This 'S.put v' is necessary to make sure that the whole vector
+  -- (not just the slices) is available -- v has already been updated
+  -- by the updates to the slices.
   S.put v
   return undefined
 -- this implementation will contain one 'fork' and one 'get'
@@ -102,17 +126,17 @@ p1 = do
 p2 :: ParVec s Float String
 p2 = do
   r <- liftST$ newSTRef "hi"
-  initTheVec 10
-  v <- getTheVec
+  initParVec 10
+  v <- getParVec
 
   elem <- liftST$ read v 5
 
   forkWithVec 5
-     (do v1 <- getTheVec
+     (do v1 <- getParVec
 --         liftST$ write v1 9 0 -- BAD! out of bounds
          liftST$ write v1 2 33.3
      )
-     (do v2 <- getTheVec
+     (do v2 <- getParVec
 --         liftST$ read v 2  -- BAD!
 --         liftST$ readSTRef r
          liftST$ write v2 2 44.4)
